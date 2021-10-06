bc5daa95ec2e2881d083301a897ac0866c441214
Fede <fedemaffi13@gmail.com>
2021-10-05 23:41:42 -0300

    05/10

--- ITBA/4-Librerias/descargaArchivo.py
+++ ITBA/4-Librerias/descargaArchivo.py
@@ -5,4 +5,4 @@ def wget(url):
     with open(url[url.rfind('/') + 1::], 'wb') as f:
         f.write(r.content)
 
-wget("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Curso_Analisis_de_Datos_Datos/Tabla1.xlsx")
+wget("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Curso_Analisis_de_Datos_Datos/Datos2.xlsx")
